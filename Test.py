0566a1ec39b7e8ee5157ec4b3a26dd5b54efd528
Rakavana <rakavana2020@gmail.com>
2019-07-09 16:04:32 +0530

    Added Code

--- Test.py
+++ Test.py
@@ -8,19 +8,19 @@ print(sys.version)
 print(sys.executable)
 
 
-# def adjacentElementsProduct(inputArray):
-#     largeValue = 0
-#     length = len(inputArray)
-#     for i in range(len(inputArray) - 1):
-#         if i != len(inputArray) - 1:
-#             largeValue1 = inputArray[i] * inputArray[i + 1]
-#             if largeValue < largeValue1:
-#                 largeValue = largeValue1
-#         else:
-#             largeValue2 = inputArray[length - 1] * inputArray[length]
-#             if largeValue < largeValue2:
-#                 largeValue = largeValue2
-#     print(largeValue)
+def adjacentElementsProduct(inputArray):
+    largeValue = 0
+    length = len(inputArray)
+    for i in range(len(inputArray) - 1):
+        if i != len(inputArray) - 1:
+            largeValue1 = inputArray[i] * inputArray[i + 1]
+            if largeValue < largeValue1:
+                largeValue = largeValue1
+        else:
+            largeValue2 = inputArray[length - 1] * inputArray[length]
+            if largeValue < largeValue2:
+                largeValue = largeValue2
+    print(largeValue)
 
 
 r = requests.get("https://www.google.com")
